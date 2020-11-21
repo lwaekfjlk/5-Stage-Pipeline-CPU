a397c72b1d7ee1714dcd2115ef2a92acb8040e04
Haofei Yu <1125027232@qq.com>
2020-11-21 23:36:48 +0800

    modify files

--- xor32.v
+++ xor32.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    23:46:31 04/05/2020 
+// Create Date:    23:06:30 02/27/2020 
 // Design Name: 
-// Module Name:    REG32 
+// Module Name:    xor32 
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -18,15 +18,9 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module REG32(input clk,
-				 input rst,
-				 input id_shouldstall,
-				 input [31:0] PC_next,
-				 output reg [31:0] PC = 0
+module xor32(input [31:0] A,
+				 input [31:0] B,
+				 output [31:0] res
     );
-always @(posedge clk or posedge rst)
-	if (rst) PC <= 0;
-	else if (id_shouldstall == 1) PC <= PC;
-		  else PC <= PC_next;
-
+assign res = A ^ B;
 endmodule
